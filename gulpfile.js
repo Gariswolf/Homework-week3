6a9e8e0ca9a44449cec7b4856c108e83c03c70ff
Garisfox <65407050+Garisfox@users.noreply.github.com>
2020-11-22 21:50:20 +0800

    gulpfile modified

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,29 @@
-'use strict'
+'use strict';
 
-const { src, dest, watch, series } = require('gulp');
+const { src, dest, watch} = require('gulp');
 const sass = require('gulp-sass');
+const uglifycss = require('gulp-uglifycss');
 
+// Compile Sass to css
 sass.compiler = require('node-sass');
-
-function compileSass(done) {
-  src('./sass/**/*.sass')
-  .pipe(sass().on('error', sass.logError))
-  .pipe(dest('./css'));
-  done();
+function compileSass(){
+    return src('./sass/**/*.sass')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest('./css'));
 }
-function watchSass() {
+
+// Uglify CSS
+function uglyCSS(){
+    return src('./css/**/*.css')
+    .pipe(uglifycss({"uglyComments": true}))
+    .pipe(dest('./dist'));
+}
+
+// Detect and modify changed file
+function watchFile(){
     watch('./sass/**/*.sass', compileSass);
+    watch('./css/**/*.css', uglyCSS)
 }
 
- exports.watchSass = watchSass
-
- const uglifycss = require('gulp-uglifycss');
-
- function uglyCSS(done){
-    src('./css/**/*.css')
-    .pipe(uglifycss({
-        "maxLineLen": 80,
-        "uglyComments": true
-    }))
-    .pipe(dest('./dist/'));
-    done();
-}
-
-function watchCSS() {
-    watch('./css/*.sass', uglyCSS);
-}
-
-exports.default = series(series(compileSass, uglyCSS), series(watchSass, watchCSS))
+// Using command "gulp" to start it 
+exports.default = watchFile
